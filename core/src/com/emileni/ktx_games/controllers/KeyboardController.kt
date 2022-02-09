ed482c955bb3ffb229887e1473d8b28d0c318dba
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-02-09 17:02:17 +0100

    do work

---
+++ core/src/com/emileni/ktx_games/controllers/KeyboardController.kt
@@ -0,0 +1,19 @@
+package com.emileni.ktx_games.controllers
+
+import com.badlogic.gdx.InputProcessor
+
+class KeyboardController : InputProcessor {
+    val up: Boolean = false
+    val down: Boolean = false
+    val right: Boolean = false
+    val left: Boolean = false
+
+    override fun keyDown(keycode: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun keyUp(keycode: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+
+}
